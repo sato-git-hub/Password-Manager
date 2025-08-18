12863aae80c606dc01a124044b8b3014455a454e
sato-git-hub <mimilili.disney@gmail.com>
2025-08-18 13:12:28 +0000

    fix: correct saving logic add: add Get Password functionality to password_manager.sh

--- password_manager.sh
+++ password_manager.sh
@@ -9,7 +9,28 @@ if [[ "$select" == "Add Password" ]]; then
  read -p "ユーザー名を入力してください：" add_user
  read -p "パスワードを入力してください：" add_password
 
+ echo "$add_service:$add_user:$add_password" >> password.txt
+
  echo "パスワードの追加は成功しました。"
  echo "Thank you!"
 
+elif [[ "$select" == "Get Password" ]]; then
+
+ read -p "サービス名を入力してください：" get_service
+
+ if grep -q "$get_service" password.txt; then
+
+IFS=":" read -r -a array <<< $(grep "$get_service" password.txt)
+
+  echo "サービス名：" ${array[0]}
+  echo "ユーザー名：" ${array[1]}
+  echo "パスワード：" ${array[2]}
+
+ else
+  echo "そのサービスは登録されていません。"
+
+ fi
+
+
 fi
+
