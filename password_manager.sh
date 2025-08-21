9fe8e4f6d872288a80bb962076f8143606773ab5
sato-git-hub <mimilili.disney@gmail.com>
2025-08-21 03:16:02 +0000

    Add:password setup on first launch and require password on startup

--- password_manager.sh
+++ password_manager.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
- echo "パスワードマネージャーへようこそ！"
+echo "パスワードマネージャーへようこそ！"
+
+#初めてパスワードマネージャーを開いたとき
+
+if [[ ! -f key.txt.gpg ]]; then
+
+    read -sp "パスワードマネージャーのパスワードを設定してください:" add_key
+    echo "$add_key" > key.txt
+    gpg --batch --yes --passphrase "$add_key" -c key.txt 2>/dev/null
+    rm -rf key.txt
+    echo "パスワードの設定が完了しました"
+    exit 0
+
+# $add_keyリセット
+
+else
+    read -sp "パスワードマネージャーのパスワードを入力してください:" key
+
+    #-d 復号内容が画面に表示
+    if gpg --batch --yes --passphrase "$key" -d key.txt.gpg > /dev/null 2>&1; then
+        echo
+        echo "パスワード認証が完了しました"
+
+    else
+        echo
+        echo "パスワード認証失敗"
+        exit 1
+    fi
+fi
 
 while true; do
  read -p "次の選択肢から入力してください(Add Password/Get Password/Exit)：" select
@@ -13,8 +41,7 @@ while true; do
 
   #password.txt.gpgが存在してたら
   if [[ -f password.txt.gpg ]]; then
-
-      echo "a" | gpg --batch --yes --passphrase-fd 0 -d password.txt.gpg > password.txt
+      echo "$key" | gpg --batch --yes --passphrase-fd 0 -d password.txt.gpg > password.txt 2>/dev/null
 
   fi
 
@@ -22,7 +49,7 @@ while true; do
 
   #password.txt.gpgに一行追加することができない
 
-  echo "a" | gpg --batch --yes --passphrase-fd 0 -c password.txt
+  echo "$key" | gpg --batch --yes --passphrase-fd 0 -c password.txt 2>/dev/null
 
   rm -rf password.txt
 
@@ -33,7 +60,7 @@ while true; do
 
   read -p "サービス名を入力してください：" get_service
 
-  echo "a" | gpg --batch --yes --passphrase-fd 0 -d password.txt.gpg > password.txt
+  echo "$key" | gpg --batch --yes --passphrase-fd 0 -d password.txt.gpg > password.txt 2>/dev/null
 
   if grep -q "^$get_service" password.txt; then
 
